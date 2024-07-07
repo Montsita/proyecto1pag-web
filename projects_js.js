06328220efa9939f63792ea28a76956386c7d3ce
MontseCobes <montsest.boi@gmail.com>
2024-07-07 20:21:09 +0200

    upgrades

--- projects_js.js
+++ projects_js.js
@@ -1,4 +1,4 @@
-async function cargarArchivoJSON1(uuid) {
+async function cargarArchivoJSON1() {
     try {
         const response = await fetch("https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects");  
       if (!response.ok) {
@@ -7,119 +7,143 @@ async function cargarArchivoJSON1(uuid) {
   
       const proyectos = await response.json();
 
+      //extraigo el numero de la id:
+
+      let params = new URLSearchParams(window.location.search);
+      let id = params.get('id');
+      let idNum = Number(id);
+
       let title = document.getElementById("titleP");
       let subtitle = document.getElementById("subTitleP");
       let date = document.getElementById("dateP");
       let imgP = document.getElementById("imageP");
       let paragraph = document.getElementById("paragraphP");
+      let link1 = document.getElementById("link1");
+      let link2 = document.getElementById("link2");
+      let link3 = document.getElementById("link3");
+      let img1 = document.getElementById("img1");
+      let img2 = document.getElementById("img2");
+      let img3 = document.getElementById("img3");
+      let title1 = document.getElementById("title1");
+      let title2 = document.getElementById("title2");
+      let title3 = document.getElementById("title3");
+      let subTitle1 = document.getElementById("subTitle1");
+      let subTitle2 = document.getElementById("subTitle2");
+      let subTitle3 = document.getElementById("subTitle3");
+      let secondUrl1 = document.getElementById("secondUrl1");
+      let secondUrl2 = document.getElementById("secondUrl2");
+      let secondUrl3 = document.getElementById("secondUrl3");
 
-      title.textContent = proyectos[3].name;
-      subtitle.textContent = proyectos[3].description;
-      date.innerHTML = `Completed on <span style="color: gray;">${proyectos[3].completed_on}</span>`;
-      imgP.src = proyectos[3].image;
-      paragraph.innerHTML = proyectos[3].content; 
+      for (let i = 0; i<=idNum; i++){
+        if(idNum === 0){
+          title.textContent = proyectos[3].name;
+          subtitle.textContent = proyectos[3].description;
+          date.innerHTML = `Completed on <span style="color: gray;">${proyectos[3].completed_on}</span>`;
+          imgP.src = proyectos[3].image;
+          paragraph.innerHTML = proyectos[3].content; 
 
-      // switch (uuid) {
-      //   case 1:
-      //     title = document.getElementById("titleP");
-      //     subtitle = document.getElementById("subTitleP");
-      //     date = document.getElementById("dateP");
-      //     imgP = document.getElementById("imageP");
-      //     paragraph = document.getElementById("paragraphP");
-      //     proyectoFiltrado = proyectos.filter(proyecto => proyecto.uuid === 1);
+          link1.href = "/projects/projects.html?id=1";
+          img1.src = proyectos[2].image;
+          title1.textContent = proyectos[2].name;
+          subTitle1.textContent = proyectos[2].description;
+          secondUrl1.href = "/projects/projects.html?id=1";
           
-      //     title.textContent = proyectoFiltrado.name;
-      //     subtitle.textContent = proyectoFiltrado.description;
-      //     date.innerHTML = `Completed on <span style="color: gray;">${proyectoFiltrado.completed_on}</span>`;      
-      //     imgP.src = proyectoFiltrado.image;
-      //     paragraph.innerHTML = proyectoFiltrado.content;
-      //     break;
+          link2.href = "/projects/projects.html?id=2";
+          img2.src = proyectos[1].image;
+          title2.textContent = proyectos[1].name;
+          subTitle2.textContent = proyectos[1].description;
+          secondUrl2.href = "/projects/projects.html?id=2";
 
-      //   case 2:
-      //     title = document.getElementById("titleP");
-      //     subtitle = document.getElementById("subTitleP");
-      //     date = document.getElementById("dateP");
-      //     imgP = document.getElementById("imageP");
-      //     paragraph = document.getElementById("paragraphP");
-      //     proyectoFiltrado = proyectos.filter(proyecto => proyecto.uuid === 2);
+          link3.href = "/projects/projects.html?id=3";
+          img3.src = proyectos[0].image;
+          title3.textContent = proyectos[0].name;
+          subTitle3.textContent = proyectos[0].description;
+          secondUrl3.href = "/projects/projects.html?id=3";
+        }
+
+        if(idNum === 1){
+          title.textContent = proyectos[2].name;
+          subtitle.textContent = proyectos[2].description;
+          date.innerHTML = `Completed on <span style="color: gray;">${proyectos[2].completed_on}</span>`;
+          imgP.src = proyectos[2].image;
+          paragraph.innerHTML = proyectos[2].content; 
+
+          link1.href = "./projects/projects.html?id=2";
+          img1.src = proyectos[1].image;
+          title1.textContent = proyectos[1].name;
+          subTitle1.textContent = proyectos[1].description;
+          secondUrl1.href = "./projects/projects.html?id=2";
           
-      //     title.textContent = proyectoFiltrado.name;
-      //     subtitle.textContent = proyectoFiltrado.description;
-      //     date.innerHTML = `Completed on <span style="color: gray;">${proyectoFiltrado.completed_on}</span>`;      
-      //     imgP.src = proyectoFiltrado.image;
-      //     paragraph.innerHTML = proyectoFiltrado.content;          
-      //     break;
+          link2.href = "./projects/projects.html?id=3";
+          img2.src = proyectos[0].image;
+          title2.textContent = proyectos[0].name;
+          subTitle2.textContent = proyectos[0].description;
+          secondUrl2.href = "./projects/projects.html?id=3";
 
-      //   case 3:
-      //     title = document.getElementById("titleP");
-      //     subtitle = document.getElementById("subTitleP");
-      //     date = document.getElementById("dateP");
-      //     imgP = document.getElementById("imageP");
-      //     paragraph = document.getElementById("paragraphP");
-      //     proyectoFiltrado = proyectos.filter(proyecto => proyecto.uuid === 3);
+          link3.href = "./projects/projects.html?id=0";
+          img3.src = proyectos[3].image;
+          title3.textContent = proyectos[3].name;
+          subTitle3.textContent = proyectos[3].description;
+          secondUrl3.href = "./projects/projects.html?id=0";
           
-      //     title.textContent = proyectoFiltrado.name;
-      //     subtitle.textContent = proyectoFiltrado.description;
-      //     date.innerHTML = `Completed on <span style="color: gray;">${proyectoFiltrado.completed_on}</span>`;      
-      //     imgP.src = proyectoFiltrado.image;
-      //     paragraph.innerHTML = proyectoFiltrado.content; 
-      //     break;
+        }
 
-      //   case 4:
-      //     title = document.getElementById("titleP");
-      //     subtitle = document.getElementById("subTitleP");
-      //     date = document.getElementById("dateP");
-      //     imgP = document.getElementById("imageP");
-      //     paragraph = document.getElementById("paragraphP");
-      //     proyectoFiltrado = proyectos.filter(proyecto => proyecto.uuid === 4);
+        if(idNum === 2){
+          title.textContent = proyectos[1].name;
+          subtitle.textContent = proyectos[1].description;
+          date.innerHTML = `Completed on <span style="color: gray;">${proyectos[1].completed_on}</span>`;
+          imgP.src = proyectos[1].image;
+          paragraph.innerHTML = proyectos[1].content; 
+
+          link3.href = "./projects/projects.html?id=3";
+          img1.src = proyectos[0].image;
+          title1.textContent = proyectos[0].name;
+          subTitle1.textContent = proyectos[0].description;
+          secondUrl1.href = "./projects/projects.html?id=3";
           
-      //     title.textContent = proyectoFiltrado.name;
-      //     subtitle.textContent = proyectoFiltrado.description;
-      //     date.innerHTML = `Completed on <span style="color: gray;">${proyectoFiltrado.completed_on}</span>`;      
-      //     imgP.src = proyectoFiltrado.image;
-      //     paragraph.innerHTML = proyectoFiltrado.content;           
-      //     break;
-      //   default:
-      //     console.log("No hay información para cargar");
-      //     break;
-      // }
+          link2.href = "./projects/projects.html?id=0";
+          img2.src = proyectos[3].image;
+          title2.textContent = proyectos[3].name;
+          subTitle2.textContent = proyectos[3].description;
+          secondUrl2.href = "./projects/projects.html?id=0";
 
-      console.log(proyectos);
+          link3.href = "./projects/projects.html?id=1";
+          img3.src = proyectos[2].image;
+          title3.textContent = proyectos[2].name;
+          subTitle3.textContent = proyectos[2].description;
+          secondUrl3.href = "./projects/projects.html?id=1";
+        }
 
-      const proyectoFiltrado = proyectos.filter(proyecto => proyecto.uuid === "1");
-  
-      console.log(proyectoFiltrado);
+        if(idNum === 3){
+          title.textContent = proyectos[0].name;
+          subtitle.textContent = proyectos[0].description;
+          date.innerHTML = `Completed on <span style="color: gray;">${proyectos[0].completed_on}</span>`;
+          imgP.src = proyectos[0].image;
+          paragraph.innerHTML = proyectos[0].content; 
+
+          link1.href = "./projects/projects.html?id=2";
+          img1.src = proyectos[1].image;
+          title1.textContent = proyectos[1].name;
+          subTitle1.textContent = proyectos[1].description;
+          secondUrl1.href = "./projects/projects.html?id=2";
+          
+          link2.href = "./projects/projects.html?id=1";
+          img2.src = proyectos[2].image;
+          title2.textContent = proyectos[2].name;
+          subTitle2.textContent = proyectos[2].description;
+          secondUrl2.href = "./projects/projects.html?id=1";
+
+          link3.href = "./projects/projects.html?id=0";
+          img3.src = proyectos[3].image;
+          title3.textContent = proyectos[3].name;
+          subTitle3.textContent = proyectos[3].description;
+          secondUrl3.href = "./projects/projects.html?id=0";
+        }
+
+      }
 
     } catch (error) {
       console.error("Error:", error);
     }
 }
-// cargarArchivoJSON1(2);
-
-// const project1 = document.querySelector('.Projecta1');
-// const project2 = document.querySelector('.Projecta2');
-// const project3 = document.querySelector('.Projecta3');
-
-// project1.addEventListener('click', function(event) {
-//     event.preventDefault(); 
-//     const uuid = "1";
-//     cargarArchivoJSON1(uuid);
-// });
-
-// project2.addEventListener('click', function(event) {
-//     event.preventDefault(); 
-//     const uuid = "2";
-//     cargarArchivoJSON1(uuid);
-// });
-
-// project3.addEventListener('click', function(event) {
-//     event.preventDefault(); 
-//     const uuid = "3";
-//     cargarArchivoJSON1(uuid);
-// });
-
-// 0 Descargarte los datos de la API
-// 1º Acceder al número de la id de la url. ?id=2 -> Quiero extraer el número 2. (Buscar como extraer info de una url)
-// 2º Crear el artículo principal usando la id 2.
-// 3º Creamos un array con los artículos restantes. Si hemos usado la id 2, creamos un array con las ids = 1, 3, 4
-// 4º Recorremos el array para crear una card por cada elemento del array.
+cargarArchivoJSON1(); 
