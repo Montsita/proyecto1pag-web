41f45214d12bf9d1a2704ab20d79acfec2e3d639
MontseCobes <montsest.boi@gmail.com>
2024-07-08 23:36:39 +0200

    changes projects

--- projects_js.js
+++ projects_js.js
@@ -6,144 +6,45 @@ async function cargarArchivoJSON1() {
       }
   
       const proyectos = await response.json();
-
+      
       //extraigo el numero de la id:
-
       let params = new URLSearchParams(window.location.search);
       let id = params.get('id');
-      let idNum = Number(id);
+
+      const bigProject = proyectos.filter((project)=>project.uuid === id)
+      const smallProjects = proyectos.filter((project)=>project.uuid != id)
+      console.log(bigProject);
+      console.log(smallProjects);
 
       let title = document.getElementById("titleP");
       let subtitle = document.getElementById("subTitleP");
       let date = document.getElementById("dateP");
       let imgP = document.getElementById("imageP");
       let paragraph = document.getElementById("paragraphP");
-      let link1 = document.getElementById("link1");
-      let link2 = document.getElementById("link2");
-      let link3 = document.getElementById("link3");
-      let img1 = document.getElementById("img1");
-      let img2 = document.getElementById("img2");
-      let img3 = document.getElementById("img3");
-      let title1 = document.getElementById("title1");
-      let title2 = document.getElementById("title2");
-      let title3 = document.getElementById("title3");
-      let subTitle1 = document.getElementById("subTitle1");
-      let subTitle2 = document.getElementById("subTitle2");
-      let subTitle3 = document.getElementById("subTitle3");
-      let secondUrl1 = document.getElementById("secondUrl1");
-      let secondUrl2 = document.getElementById("secondUrl2");
-      let secondUrl3 = document.getElementById("secondUrl3");
+        
+      title.textContent = bigProject[0].name;
+      subtitle.textContent = bigProject[0].description;
+      date.innerHTML = `Completed on <span style="color: gray;">${bigProject[0].completed_on}</span>`;
+      imgP.src = bigProject[0].image;
+      paragraph.innerHTML = bigProject[0].content; 
 
-      for (let i = 0; i<=idNum; i++){
-        if(idNum === 0){
-          title.textContent = proyectos[3].name;
-          subtitle.textContent = proyectos[3].description;
-          date.innerHTML = `Completed on <span style="color: gray;">${proyectos[3].completed_on}</span>`;
-          imgP.src = proyectos[3].image;
-          paragraph.innerHTML = proyectos[3].content; 
+      const sectionProjects = document.querySelector(".article-projects");
 
-          link1.href = "/projects/projects.html?id=1";
-          img1.src = proyectos[2].image;
-          title1.textContent = proyectos[2].name;
-          subTitle1.textContent = proyectos[2].description;
-          secondUrl1.href = "/projects/projects.html?id=1";
-          
-          link2.href = "/projects/projects.html?id=2";
-          img2.src = proyectos[1].image;
-          title2.textContent = proyectos[1].name;
-          subTitle2.textContent = proyectos[1].description;
-          secondUrl2.href = "/projects/projects.html?id=2";
-
-          link3.href = "/projects/projects.html?id=3";
-          img3.src = proyectos[0].image;
-          title3.textContent = proyectos[0].name;
-          subTitle3.textContent = proyectos[0].description;
-          secondUrl3.href = "/projects/projects.html?id=3";
-        }
-
-        if(idNum === 1){
-          title.textContent = proyectos[2].name;
-          subtitle.textContent = proyectos[2].description;
-          date.innerHTML = `Completed on <span style="color: gray;">${proyectos[2].completed_on}</span>`;
-          imgP.src = proyectos[2].image;
-          paragraph.innerHTML = proyectos[2].content; 
-
-          link1.href = "./projects/projects.html?id=2";
-          img1.src = proyectos[1].image;
-          title1.textContent = proyectos[1].name;
-          subTitle1.textContent = proyectos[1].description;
-          secondUrl1.href = "./projects/projects.html?id=2";
-          
-          link2.href = "./projects/projects.html?id=3";
-          img2.src = proyectos[0].image;
-          title2.textContent = proyectos[0].name;
-          subTitle2.textContent = proyectos[0].description;
-          secondUrl2.href = "./projects/projects.html?id=3";
-
-          link3.href = "./projects/projects.html?id=0";
-          img3.src = proyectos[3].image;
-          title3.textContent = proyectos[3].name;
-          subTitle3.textContent = proyectos[3].description;
-          secondUrl3.href = "./projects/projects.html?id=0";
-          
-        }
-
-        if(idNum === 2){
-          title.textContent = proyectos[1].name;
-          subtitle.textContent = proyectos[1].description;
-          date.innerHTML = `Completed on <span style="color: gray;">${proyectos[1].completed_on}</span>`;
-          imgP.src = proyectos[1].image;
-          paragraph.innerHTML = proyectos[1].content; 
-
-          link3.href = "./projects/projects.html?id=3";
-          img1.src = proyectos[0].image;
-          title1.textContent = proyectos[0].name;
-          subTitle1.textContent = proyectos[0].description;
-          secondUrl1.href = "./projects/projects.html?id=3";
-          
-          link2.href = "./projects/projects.html?id=0";
-          img2.src = proyectos[3].image;
-          title2.textContent = proyectos[3].name;
-          subTitle2.textContent = proyectos[3].description;
-          secondUrl2.href = "./projects/projects.html?id=0";
-
-          link3.href = "./projects/projects.html?id=1";
-          img3.src = proyectos[2].image;
-          title3.textContent = proyectos[2].name;
-          subTitle3.textContent = proyectos[2].description;
-          secondUrl3.href = "./projects/projects.html?id=1";
-        }
-
-        if(idNum === 3){
-          title.textContent = proyectos[0].name;
-          subtitle.textContent = proyectos[0].description;
-          date.innerHTML = `Completed on <span style="color: gray;">${proyectos[0].completed_on}</span>`;
-          imgP.src = proyectos[0].image;
-          paragraph.innerHTML = proyectos[0].content; 
-
-          link1.href = "./projects/projects.html?id=2";
-          img1.src = proyectos[1].image;
-          title1.textContent = proyectos[1].name;
-          subTitle1.textContent = proyectos[1].description;
-          secondUrl1.href = "./projects/projects.html?id=2";
-          
-          link2.href = "./projects/projects.html?id=1";
-          img2.src = proyectos[2].image;
-          title2.textContent = proyectos[2].name;
-          subTitle2.textContent = proyectos[2].description;
-          secondUrl2.href = "./projects/projects.html?id=1";
-
-          link3.href = "./projects/projects.html?id=0";
-          img3.src = proyectos[3].image;
-          title3.textContent = proyectos[3].name;
-          subTitle3.textContent = proyectos[3].description;
-          secondUrl3.href = "./projects/projects.html?id=0";
-        }
-
-      }
-
+      smallProjects.forEach(project => {
+        sectionProjects.innerHTML += `
+            <a id="link1" href="../projects/projects.html?id=${project.uuid}">
+              <div class="projects">  
+                  <img id="img1" src="${project.imageURL}" alt="project-image2">
+                  <h3 id="title1">${project.name}</h3>
+                  <h4 id="subTitle1">${project.description}</h4>
+                  <a id="secondUrl1" class= "project-learn"href="../projects/projects.html?id=${project.uuid}">Learn more</a>
+              </div>
+            </a>
+        `
+      });
+    
     } catch (error) {
       console.error("Error:", error);
     }
 }
-cargarArchivoJSON1(); 
+cargarArchivoJSON1()
